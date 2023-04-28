48a451be66a278622ea4b49822b56a7998d14278
jacoyutorius <jacoyutorius@gmail.com>
2023-04-29 07:26:13 +0900

    プロジェクト名を変更した

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Amplify React Sandbox</h1>
+        <h1>Amplify Toybox</h1>
       </header>
 
       <section>
