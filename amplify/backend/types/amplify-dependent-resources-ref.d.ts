bcd7235495fe91bb3b66348980ba5aa7afa3701e
jacoyutorius <jacoyutorius@gmail.com>
2023-06-29 04:41:28 +0900

    analytics

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,15 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "analytics": {
+    "amplifytoybox": {
+      "Id": "string",
+      "Region": "string",
+      "appName": "string"
+    }
+  },
+  "auth": {
+    "amplifytoybox": {
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string"
+    }
+  }
+}
